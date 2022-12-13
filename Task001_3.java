d19913d249c7c954e0ed542c7bac7798e6788725
Дмитрий Мачнев <dam-11-08-88@mail.ru>
2022-12-13 12:05:26 +0500

    Кодировка в задании 3 исправлена

--- Task001_3.java
+++ Task001_3.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Task001_3 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.print("¬ведите первое число: ");
         int a = in.nextInt();
